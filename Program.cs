b7c8acc37cd8d107644c947ee91af39c3b919150
Tobias Zürcher <tobias.zuercher@gmail.com>
2016-11-30 10:07:11 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static  void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MainAsync(args).Wait();
+        }
+
+        private static async void MainAsync(string[] args)
+        {
+            var client = new HttpClient();
+            
+        }
+    }
+}
